3951bc1efce4e17b830736bdfcc2b84ab9b13e3b
YuFen <yufenhuangtaiwan.com>
2022-03-14 23:38:00 +0800

    Models

--- app/Models/Animal.php
+++ app/Models/Animal.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Animal extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'type_id',
+        'name',
+        'birthday',
+        'area',
+        'fix',
+        'description',
+        'personality',
+        'user_id', //不建議允許批量寫入，將在後續身分驗證章節修改這邊的設定
+    ];
 }
